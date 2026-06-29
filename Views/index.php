5826df2a1cc6a6c82fe1c53675d3a85df57dd97f
Estefany Ortiz <kendryher14@gmail.com>
2026-06-29 15:05:15 -0600

    eso no me deja hacer nada

--- Views/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Redirigir automáticamente a la pantalla de login
-header("C:\xampp\htdocs\Farmacia");//Estos lo tenes que cambiar
-exit();
-?>
---
+++ index.php/index.php
@@ -0,0 +1,5 @@
+<?php
+// Este archivo está afuera y manda automáticamente al login que está adentro
+header("Location: Views/login.php");
+exit();
+?>
